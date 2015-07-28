44c669e365fb73ebe94193a6babe885bcc743ace
sepehr ansaripour <ansaripours@Ansaripours-PC.senet-int.com>
2015-07-28 10:55:23 -0400

    minor tidying up

--- IPH.py
+++ IPH.py
@@ -1,12 +1,13 @@
 import argparse
-parser = argparse.ArgumentParser(prog="IPSorter",usage="%(prog)s [options]",description="Finds IP addresses, outputs a CSV file of your choosing (private, public, or both)\n"+"Example: -p -i file.csv -o file2.csv")
-parser.add_argument("-p",action='store_true',help ="gets private") #flag types... 'store_true' gives flag a default value (of False) so that it doesn't expect an argument
+parser = argparse.ArgumentParser(prog="IPSorter",usage="%(prog)s [options]",description="Finds IP addresses, outputs a"
+                       " CSV file of your choosing (private, public, or both)\n"+"Example: -p -i file.csv -o file2.csv")# flag types... 'store_true' gives flag a default value (of False) so that it doesn't expect an argument
+parser.add_argument("-p",action='store_true',help ="gets private")
 parser.add_argument("-u",action='store_true',help="gets public")
 parser.add_argument("-pu",action='store_true',help="gets both public and private")
 parser.add_argument("-o",help="type output file name, ex: file.csv")
 parser.add_argument("-i",help="type input file name, ex: file.csv")
-args = parser.parse_args() #parameters passed in after flag
-def IP_Checker(ipList): #checks if there are sufficient octets in the IP
+args = parser.parse_args()                                                                                              # parameters passed in after flag
+def IP_Checker(ipList):                                                                                                 # checks if there are sufficient octets in the IP
     try:
         octetCount = 0
         for i in ipList:
@@ -18,7 +19,7 @@ def IP_Checker(ipList): #checks if there are sufficient octets in the IP
             return False
     except:
         print("IP_Checker Exception")
-def IP_Formatter(ipList): #checks if public IP
+def IP_Formatter(ipList):                                                                                               # checks if public IP
     try:
         ipFormatList = []
         octet = ''
@@ -31,7 +32,7 @@ def IP_Formatter(ipList): #checks if public IP
                 ipFormatList.append(octet)
                 octet = ''
                 ipFormatList.append(i)
-            if index+1 == len(ipList): #appends the last octet
+            if index+1 == len(ipList):                                                                                  # appends the last octet
                 ipFormatList.append(octet)
         return ipFormatList
     except:
@@ -54,30 +55,30 @@ def publicIP_Checker(ipList):
     except:
         print("publicIP_Checker Exception")
 def IP_Finder():
-    listAll = [] #used to store allText so that it can be iterated
-    ipList = [] #stores an IP or w/e comes its way, is then used to check if it contains a valid IP, and it is reset after each IP, line 110
-    ipList_True = [] #valid IP addresses stored here
+    listAll = []                                                                                                        # used to store allText so that it can be iterated
+    ipList = []                                                                                                         # stores an IP or w/e comes its way, is then used to check if it contains a valid IP, and it is reset after each IP, line 110
+    ipList_True = []                                                                                                    # valid IP addresses stored here
     index = -1
-    octetCount = 0 #stores the counted number of octets in IP address
-    pCount = 0 #stores the counted number of periods
-    strIP = '' #stores an IP as a string and then is reset, line 140
-    strIP_List = [] #stores each completed IP as a string, aka strIP
-    indexList = [] #stores the index for '.'
+    octetCount = 0                                                                                                      # stores the counted number of octets in IP address
+    pCount = 0                                                                                                          # stores the counted number of periods
+    strIP = ''                                                                                                          # stores an IP as a string and then is reset, line 140
+    strIP_List = []                                                                                                     # stores each completed IP as a string, aka strIP
+    indexList = []                                                                                                      # stores the index for '.'
     check = bool
-    falsecount=0 #counts each time a unit after a '.' from range [2,5] is not a period, line
+    falsecount=0                                                                                                        # counts each time a unit after a '.' from range [2,5] is not a period, line
     if args.i != None and "xls" not in (args.i).split('.')[-1]:
-        f = open(str(args.i)) #gets the input file name from -i flag
+        f = open(str(args.i))                                                                                           # gets the input file name from -i flag
     elif "xls" in (args.i).split('.')[-1]:
-        from xlrd import * #imports the needed library to read .xlsx files
+        from xlrd import *                                                                                              # imports the needed library to read .xlsx files
         aString = open_workbook(args.i)
         f = open("csvFile.csv","wb")
         for s in aString.sheets():
-            f.write(bytes('Sheet:','utf-8')+bytes(s.name,'utf-8')+bytes("\n",'utf-8')) #writes sheet name
+            f.write(bytes('Sheet:','utf-8')+bytes(s.name,'utf-8')+bytes("\n",'utf-8'))                                  # writes sheet name
             for row in range(s.nrows):
                 values = []
                 for col in range(s.ncols):
                     values.append(s.cell(row,col).value)
-                f.write(bytes(str(values),'utf-8')) #writes the values per sheet to csvFile
+                f.write(bytes(str(values),'utf-8'))                                                                     # writes the values per sheet to csvFile
         f.close()
         f = open("csvFile.csv")
     else:
@@ -86,17 +87,17 @@ def IP_Finder():
         allText = f.read()
     except Exception as e:
         print("READING FAILED: "+str(e))
-    for i in allText: #appends all the text into a list, character by character
+    for i in allText:                                                                                                   # appends all the text into a list, character by character
         listAll.append(i)
-    for i in listAll: #iterates through the list-form of the text
+    for i in listAll:                                                                                                   # iterates through the list-form of the text
         index += 1
-        if i == '.': #finds period, then gets the first three octets of IP
-            try:  # tries each unit of the list before a '.', up to 3, to check if integer
+        if i == '.':                                                                                                    # if '.' found, then gets the first three octets of IP
+            try:                                                                                                        # tries each unit of the list before a '.', up to 3, to check if integer
                 pCount=0
                 falsecount = 0
-                a = int(listAll[index - 1])  # checking unit before '.'
-                if index - 2 >= 0:  # to avoid referring to the first unit of end of list
-                    b = int(listAll[index - 2])  # checking 2 units before '.', etc...
+                a = int(listAll[index - 1])                                                                             # checking unit before '.'
+                if index - 2 >= 0:                                                                                      # to avoid referring to the first unit of end of list
+                    b = int(listAll[index - 2])                                                                         # checking 2 units before '.', etc...
                 else:
                     raise Exception
                 if index - 3 >= 0:
@@ -115,7 +116,7 @@ def IP_Finder():
                 else:
                     raise Exception
             except:
-                try: # tries each unit of the list before a '.', up to 2, to check if integer
+                try:                                                                                                    # tries each unit of the list before a '.', up to 2, to check if integer
                     pCount = 0
                     falsecount = 0
                     a = int(listAll[index - 1])
@@ -141,7 +142,7 @@ def IP_Finder():
                     else:
                         raise Exception
                 except:
-                    try: # tries each unit of the list before a '.', up to 1, to check if integer
+                    try:                                                                                                # tries each unit of the list before a '.', up to 1, to check if integer
                         pCount = 0
                         falsecount = 0
                         a = int(listAll[index - 1])
@@ -161,21 +162,21 @@ def IP_Finder():
                             ipList.append(a);ipList.append('.')
                             octetCount += 1
                     except:
-                        continue  # continues to next iteration if none of the units before '.' were integers
-            if i == '.' and octetCount % 3 == 0 and octetCount != 0:  # gets last octet of IP, also checks if this completed IP is valid and public
-                try:  # tries each unit after the last '.' in the address, up to 3, to check if integer
+                        continue                                                                                        # continues to next iteration if none of the units before '.' were integers
+            if i == '.' and octetCount % 3 == 0 and octetCount != 0:                                                    # gets last octet of IP, also checks if this completed IP is valid and public
+                try:                                                                                                    # tries each unit after the last '.' in the address, up to 3, to check if integer
                     a = int(listAll[index + 1])
                     b = int(listAll[index + 2])
                     c = int(listAll[index + 3])
-                    ipList.append(a);ipList.append(b);ipList.append(c) # this is the last octet so no '.' is appended
-                    if IP_Checker(ipList) == True:  # checks if valid IP
+                    ipList.append(a);ipList.append(b);ipList.append(c)                                                  # this is the last octet so no '.' is appended
+                    if IP_Checker(ipList) == True:                                                                      # checks if valid IP
                         ipList_True.append(ipList)
                         if args.u:
-                            if publicIP_Checker(ipList) == True:  # checks if public IP
-                                for i in ipList:  # makes into a string ex: [1,2,0,'.',2,'.',1,'.',10] will be '120.2.1.10'
+                            if publicIP_Checker(ipList) == True:                                                        # checks if public IP
+                                for i in ipList:                                                                        # makes into a string ex: [1,2,0,'.',2,'.',1,'.',10] will be '120.2.1.10'
                                     strIP += str(i)
-                                strIP_List.append(strIP)  # appends the completed IP string to a list
-                                strIP = ''  # resets the completed IP string so that next IP can be added to empty string again
+                                strIP_List.append(strIP)                                                                # appends the completed IP string to a list
+                                strIP = ''                                                                              # resets the completed IP string so that next IP can be added to empty string again
                         elif args.p:
                             if publicIP_Checker(ipList) == False:
                                 for i in ipList:
@@ -245,7 +246,7 @@ def IP_Finder():
 def PublicIP_Result():
     strIP_List = IP_Finder()
     if args.o:
-        f = open(str(args.o),'wb') #creates a new file with the name given by -o flag... 'wb' tells it to create if not existing and overwrite if existing
+        f = open(str(args.o),'wb')                                                                                      # creates a new file with the name given by -o flag... 'wb' tells it to create if not existing and overwrite if existing
     else:
         try:
             raise Exception
